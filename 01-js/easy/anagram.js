4cc0fc3b8d51718f5a1fab86965a169e2f63de6b
akshaysrivastava113 <akshay.srivastava113@gmail.com>
2023-12-09 15:32:54 +0530

    new commit

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,25 +5,29 @@
 */
 
 function isAnagram(str1, str2) {
-  str1 = str1.toLowerCase();
-  str2 = str2.toLowerCase();
-  if(str1.length === str2.length){
-    let ascSum1 = 0;
-    let ascSum2 = 0;
-    for(let i=0; i < str1.length; i++){
-      ascSum1+=str1.charCodeAt(i);
-      ascSum2+=str2.charCodeAt(i);
-    }
-    // console.log(ascSum1);
-    // console.log(ascSum2);
-    if(ascSum1 === ascSum2){
-      return true;
-    } else {
-      return false;
-    }
-  } else {
-    return false;
-  }
+  // str1 = str1.toLowerCase();
+  // str2 = str2.toLowerCase();
+  // if(str1.length === str2.length){
+  //   let ascSum1 = 0;
+  //   let ascSum2 = 0;
+  //   for(let i=0; i < str1.length; i++){
+  //     ascSum1+=str1.charCodeAt(i);
+  //     ascSum2+=str2.charCodeAt(i);
+  //   }
+  //   // console.log(ascSum1);
+  //   // console.log(ascSum2);
+  //   if(ascSum1 === ascSum2){
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // } else {
+  //   return false;
+  // }
+  let someStr = "Hello World";
+  let sliced = someStr.indexOf("ell");
+  console.log(typeof sliced);
+  console.log(someStr);
 }
-
+isAnagram();
 module.exports = isAnagram;
