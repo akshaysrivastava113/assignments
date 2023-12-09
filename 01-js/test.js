eedc899c1418233a17a68bbdbd21c48481a58293
akshaysrivastava113 <akshay.srivastava113@gmail.com>
2023-12-09 20:52:10 +0530

    Add a.txt

---
+++ 01-js/test.js
@@ -0,0 +1,30 @@
+// class Contact{
+//     constructor(ContactType, ContactFname, ContactLname, ContactEmail, ContactAge){
+//         this.ContactType = ContactType;        
+//         this.ContactFname = ContactFname;
+//         this.ContactLname = ContactLname;
+//         this.ContactEmail = ContactEmail;
+//         this.ContactAge = ContactAge;
+//     }
+//     static isSFObj() {
+//         return true; 
+//     }
+//     getFullName(separator) {
+//         console.log(this);
+//         return this.ContactFname+separator+this.ContactLname;
+//     }
+// }
+
+// let tc = new Contact("tc", "Akshay", "Srivastava", "aksh@test.com", 27);
+// let nontc = new Contact("nontc", "Akash", "Singh", "akash@test.com", 23);
+// // console.log(getFullName());
+
+// let someObj = {
+//     name: 'Akshay',
+//     age: 27
+// }
+
+
+// console.log(typeof tc);
+// console.log(typeof someObj);
+
