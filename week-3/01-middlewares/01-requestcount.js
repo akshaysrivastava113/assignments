37f4d51e6f5da933f9f3545066098b2491e5ce16
akshaysrivastava113 <akshay.srivastava113@gmail.com>
2023-12-25 18:43:17 +0530

    Solve requestCount.js

--- week-3/01-middlewares/01-requestcount.js
+++ week-3/01-middlewares/01-requestcount.js
@@ -1,6 +1,6 @@
-const request = require('supertest');
-const assert = require('assert');
-const express = require('express');
+const request = require("supertest");
+const assert = require("assert");
+const express = require("express");
 
 const app = express();
 let requestCount = 0;
@@ -10,16 +10,32 @@ let requestCount = 0;
 // maintain a count of the number of requests made to the server in the global
 // requestCount variable
 
-app.get('/user', function(req, res) {
-  res.status(200).json({ name: 'john' });
+function requestsLogger(req,res,next) {
+  let newCount = ++requestCount;
+  console.log(newCount);
+  next();
+}
+
+app.use(requestsLogger);
+
+app.get("/", (req, res) => {
+  res.send("Deployed");
 });
 
-app.post('/user', function(req, res) {
-  res.status(200).json({ msg: 'created dummy user' });
+app.get("/user", function (req, res) {
+  res.status(200).json({ name: "john" });
 });
 
-app.get('/requestCount', function(req, res) {
+app.post("/user", function (req, res) {
+  res.status(200).json({ msg: "created dummy user" });
+});
+
+app.get("/requestCount", function (req, res) {
   res.status(200).json({ requestCount });
+});
+
+app.listen(3000, function () {
+  console.log("Listening on 3000");
 });
 
 module.exports = app;
